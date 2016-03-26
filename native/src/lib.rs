ebb13a89263dd70fb70bc8db3cb3a03c20ef9261
tillarnold <throwable42@gmail.com>
2016-03-27 00:12:27 +0100

    Throw actual error on error

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -11,20 +11,21 @@ use neon::mem::Handle;
 fn left_pad(call: Call) -> JsResult<JsString> {
     let scope = call.scope;
     let str_h: Handle<JsString> = try!(try!(call.arguments.require(scope, 0)).check::<JsString>());
-    let len_h: Handle<JsInteger> = try!(try!(call.arguments.require(scope, 1)).check::<JsInteger>());
+    let len_h: Handle<JsInteger> = try!(try!(call.arguments.require(scope, 1))
+                                            .check::<JsInteger>());
     let ch_h: Handle<JsString> = try!(try!(call.arguments.require(scope, 2)).check::<JsString>());
-    
+
     let string = (*str_h).value();
     let ch = (*ch_h).value();
-    let len : u32 = (*len_h).value() as u32;
-   
-    if let Ok(result) = left_pad_io::left_pad(&string, &ch, len) {
-      Ok(JsString::new(scope, &result).unwrap())
+    let len: u32 = (*len_h).value() as u32;
+
+    let res = left_pad_io::left_pad(&string, &ch, len);
+    if let Ok(result) = res {
+        Ok(JsString::new(scope, &result).unwrap())
+    } else {
+        neon::js::error::throw(JsString::new(scope, &format!("{:?}", res)).unwrap())
     }
-    else {
-      Err(neon::vm::Throw)
-    }
-    
+
 }
 
 register_module!(m, {
